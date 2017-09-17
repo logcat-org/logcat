580a3569ac91b2d7739938ef31e0ead289017ff4
spirinvladimir <spirinvladimir@gmail.com>
2017-09-17 07:41:24 +0300

    Web frontend moved to  @logcat-org/web-log

--- index.js
+++ index.js
@@ -2,32 +2,20 @@ const
 	util = require('util'),
 	spawn = require('child_process').spawn,
 	check = require('./lib/check'),
-	lines = require('./lib/lines'),
-	web = require('./lib/web'),
-	console = require('console-logger-api');
+	lines = require('./lib/lines');
 
-module.exports = (command) => {
+module.exports = (command, plugins) => {
 	const
-		server = web(),
-		onError = (data) => {
-			lines(data).forEach((line) => {
-				console.error(line);
-				server.error(line);
-			});
-		},
+		onError = data =>
+			lines(data).forEach(line =>
+				plugins.forEach(plugin => plugin.error(line))),
 		args = command.split(' '),
 		logcat = spawn(args.shift(), args);
 
-	logcat.stdout.on('data', (data) => {
-		check(lines(data)).forEach((lineWithType) => {
-			const
-				line = lineWithType.line,
-				type = lineWithType.type;
-
-			console[type](line);
-			server[type](line);
-		});
-	});
+	logcat.stdout.on('data', data =>
+		check(lines(data)).forEach(lineWithType =>
+			plugins.forEach(plugin =>
+				plugin[lineWithType.type](lineWithType.line))));
 
 	logcat.stderr.on('data', onError);
 	logcat.on('error', onError);
